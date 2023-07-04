5598b2d7b024cfd8404475e6fa26dfa16838ef92
David Castillo <davidcastillo@davids-air.lan>
2023-07-04 15:29:56 -0600

    gatsby tutorial pt 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import {StaticImage} from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Bose, in his natural habitat - a puppy park"
+        src="../images/bose.jpg"
+      />
     </Layout>
   )
 };
